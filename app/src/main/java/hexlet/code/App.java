1fb2f426d1cbec00822d8aba5173af0ee11968eb
Akbarchick06 <akbarkabylzanov6@gmail.com>
2025-05-01 16:56:43 +0300

    7th add class progression

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,16 +2,21 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.NOD;
+import hexlet.code.games.Progression; // Импортируем класс Progression
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!"); // Добавлено
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression"); // Добавлена опция для Progression
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -26,10 +31,29 @@ public class App {
                 Cli.greetUser();
                 break;
             case 2:
-                Engine.runGame(Even.getGameInstruction(), new Even()); //  Запускаем игру Even
+                if (Cli.name == null) {
+                    Cli.greetUser();
+                }
+                Engine.runGame(Even.getGameInstruction(), new Even());
                 break;
             case 3:
-                Engine.runGame(Calc.getGameInstruction(), new Calc()); // Запускаем игру Calc
+                if (Cli.name == null) {
+                    Cli.greetUser();
+                }
+                Engine.runGame(Calc.getGameInstruction(), new Calc());
+                break;
+            case 4:
+                if (Cli.name == null) {
+                    Cli.greetUser();
+                }
+                Engine.runGame(NOD.getGameInstruction(), new NOD()
+                );
+                break;
+            case 5: // Добавлена обработка выбора для Progression
+                if (Cli.name == null) {
+                    Cli.greetUser();
+                }
+                Engine.runGame(Progression.getGameInstruction(), new Progression()); // Запускаем игру Progression
                 break;
             default:
                 System.out.println("Invalid choice. Please try again.");
